0a96fe644ab815c722bc0d822ba2b8274a759f6f
markhibberd <mark@hibberd.id.au>
2014-11-12 14:44:24 +1100

    moar

--- ivory-storage/src/main/scala/com/ambiata/ivory/storage/manifest/FactsetManifest.scala
+++ ivory-storage/src/main/scala/com/ambiata/ivory/storage/manifest/FactsetManifest.scala
@@ -10,9 +10,23 @@ import scalaz._
 case class FactsetManifest(core: Manifest, id: FactsetId, data: FactsetDataVersion, partitions: List[Partition])
 
 object FactsetManifest {
+  def location(repository: Repository, id: FactsetId): IvoryLocation =
+    repository.toIvoryLocation(Repository.factset(id)) </> FileName.unsafe(".manifest.json")
+
   def writeWith(repository: Repository, id: FactsetId, data: FactsetDataVersion, partitions: List[Partition]): ResultTIO[Unit] =
-    IvoryLocation.writeUtf8(repository.toIvoryLocation(Repository.factset(id)) </> FileName.unsafe(".manifest.json"),
-      FactsetManifest(Manifest(ManifestVersion.V1, IvoryVersion.get), id, data, partitions).asJson.spaces2)
+    write(repository, FactsetManifest(Manifest(ManifestVersion.V1, IvoryVersion.get), id, data, partitions))
+
+  def write(repository: Repository, manifest: FactsetManifest): ResultTIO[Unit] =
+    IvoryLocation.writeUtf8(location(repository, manifest.id), manifest.asJson.spaces2)
+
+  def read(repository: Repository, id: FactsetId): ResultTIO[Option[FactsetManifest]] =
+    IvoryLocation.readUtf8(location(repository, id)).map(s => s.decodeOption[FactsetManifest])
+
+  def readOrFail(repository: Repository, id: FactsetId): ResultTIO[FactsetManifest] =
+    IvoryLocation.readUtf8(location(repository, id)).flatMap(s => ResultT.fromDisjunctionString(s.decodeEither[FactsetManifest]))
+
+  def exists(repository: Repository, id: FactsetId): ResultTIO[Boolean] =
+    IvoryLocation.exists(location(repository, id))
 
   implicit def FactsetManifestEqual: Equal[FactsetManifest] =
     Equal.equalA[FactsetManifest]
