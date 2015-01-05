36c1978b8d16d382018e03de63db8cf23efc0020
Charles O'Farrell <charleso@charleso.org>
2015-01-05 16:16:07 +1100

    fixup! Reduce size of desc/tombstones and list values

--- ivory-core/src/test/scala/com/ambiata/ivory/core/gen/GenPlus.scala
+++ ivory-core/src/test/scala/com/ambiata/ivory/core/gen/GenPlus.scala
@@ -10,7 +10,7 @@ object GenPlus {
     Gen.sized(n => Gen.choose(1, Math.max(1, n)).flatMap(i => Gen.listOfN(i, gen)))
 
   def sizedList[A](l: Int, n: Int, gen: => Gen[A]): Gen[List[A]] =
-    Gen.sized(n => Gen.choose(l, Math.max(l, n)).flatMap(i => Gen.listOfN(i, gen)))
+    Gen.sized(n => Gen.choose(l, Math.min(l, n)).flatMap(i => Gen.listOfN(i, gen)))
 
   // The Gen version of this function resulted in discarded tests
   def posNum[A](implicit num: Numeric[A], c: Choose[A]): Gen[A] =
