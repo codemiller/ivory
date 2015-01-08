7ce0bfe42cb32ba3fd2e7042cda686292ced7bf0
Russell Aronson <russell.aronson@gmail.com>
2015-01-08 11:15:20 +1100

    Add new version to SnapshotFormat

--- ivory-core/src/main/scala/com/ambiata/ivory/core/SnapshotFormat.scala
+++ ivory-core/src/main/scala/com/ambiata/ivory/core/SnapshotFormat.scala
@@ -12,6 +12,10 @@ object SnapshotFormat {
       bytes value with no partitioning. */
   case object V1 extends SnapshotFormat
 
+  /** V2 is partitioned sequence files by namespace. The key is the fact date stored as an
+      integer, and the value is the "thrift-fact" stored as bytes. */
+  case object V2 extends SnapshotFormat
+
   /* NOTE Don't forget the arbitrary if you add a format. */
 
   implicit def SnapshotFormatEqual: Equal[SnapshotFormat] =
@@ -20,7 +24,9 @@ object SnapshotFormat {
   implicit def SnapshotFormatCodecJson: CodecJson[SnapshotFormat] =
     ArgonautPlus.codecEnum("SnapshotFormat", {
       case V1 => "v1"
+      case V2 => "v2"
     }, {
       case "v1" => V1
+      case "v2" => V2
     })
 }
